5b97b275a5a68ed6f67a7e4dc75b4101c9de5a01
KaveriRaut <rautkaveri02@gmail.com>
2024-01-03 20:12:49 +0530

    First Commit: Preprocessing done, training dataset creation done, SVM model training in stage 1

---
+++ code/fuzzy_logic.py
@@ -0,0 +1,19 @@
+from thefuzz import fuzz, process
+
+s1="Hello World"
+s2="Hello Hello world world world"
+
+print(fuzz.ratio(s1, s2))
+
+print(fuzz.partial_ratio(s1, s2))
+
+print(fuzz.token_sort_ratio(s1,s2))
+
+print(fuzz.token_set_ratio(s1,s2))
+
+
+things=['Programming language', "Nativ Language", "React Native", "Some Stuff", "Hello world", "Coding stuff"]
+
+print(process.extract("language", things, limit=2))
+
+print(process.extract("stuff some", things, limit=2, scorer=fuzz._token_sort_ratio))
